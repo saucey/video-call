1241b746bd318dc8871ab41e9a4fbefc2326f7a8
lsnomensa <lscott@nomensa.com>
2025-07-05 16:55:53 +0100

    env change

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import http from "http";
 import cors from "cors";
 import { Server, Socket } from "socket.io";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 app.use(cors());
